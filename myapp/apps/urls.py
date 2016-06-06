1138d54fd0f80647fc180d0656eded459b67ddd2
Jose Miguel <josemiguel.navarro@bbva.com>
2016-06-06 19:41:00 +0200

    added templates and front logic

--- myapp/apps/urls.py
+++ myapp/apps/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<app_id>[0-9]+)$', views.detail, name='detail'),
+    url(r'^edit/(?P<app_id>[0-9]+)$', views.edit, name='edit'),
+    url(r'^upload$', views.upload, name='upload'),
+    url(r'^list/public$', views.public, name='public'),
+    url(r'^list/private$', views.private, name='private'),
 ]
--- myapp/apps/views.py
+++ myapp/apps/views.py
@@ -1,4 +1,60 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import App
+
+def print_response(request, template_path, context):
+	template = loader.get_template(template_path)
+	return HttpResponse(template.render(context, request))
 
 def index(request):
-    return HttpResponse("APPS application")
+	apps = App.objects.all()
+	context = {
+		'apps': apps,
+		'title': 'Apps'
+	}
+	template_path = 'apps/index.html'
+	return print_response(request,template_path,context)
+
+def upload(request):
+	template_path = 'apps/upload.html'
+	context = {
+		'title': 'Upload'
+	}
+	return print_response(request,template_path,context)
+
+def detail(request, app_id):
+	template_path = 'apps/detail.html'
+	app = App.objects.get(id=app_id)
+	template = loader.get_template('apps/detail.html')
+	context = {
+		'app': app,
+		'id':app_id,
+	}
+	return print_response(request,template_path,context)
+
+def edit(request, app_id):
+	template_path = 'apps/edit.html'
+	app = App.objects.get(id=app_id)
+	context = {
+		'app': app,
+		'title': 'Edit'
+	}
+	return print_response(request,template_path,context)
+
+def public(request):
+	template_path = 'apps/index.html'
+	public_apps = App.objects.filter(access_right='PU')
+	context = {
+		'apps': public_apps,
+		'title': 'Public apps'
+	}
+	return print_response(request,template_path,context)
+
+def private(request):	
+	template_path = 'apps/index.html'
+	private_apps = App.objects.filter(access_right='PR')
+	context = {
+		'apps': private_apps,
+		'title': 'Private apps'
+	}
+	return print_response(request,template_path,context)
